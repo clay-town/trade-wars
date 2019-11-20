10416bd918685a35cfd0776d82d79f7bee31de75
priscillashort <pshort2@cocc.edu>
2019-11-19 20:39:20 -0800

    Change to static site to display css on heroku

--- cmd/tradewars/main.go
+++ cmd/tradewars/main.go
@@ -4,36 +4,49 @@ import (
     "log"
     "net/http"
     "os"
+    //"github.com/gorilla/mux"
 )
 
-/*func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/grid.html", grid)
-    mux.HandleFunc("/trade.html", trade)
-    mux.HandleFunc("/chat.html", chat)
-    mux.HandleFunc("/snippet", showSnippet)
-    mux.HandleFunc("/snippet/create", createSnippet)
-
-    log.Println("Starting server on :" + os.Getenv("PORT"))
-    err := http.ListenAndServe(os.Getenv("CHROMEHOST") + ":" + os.Getenv("PORT"), mux)
-    log.Fatal(err)
-}*/
-
 func main() {
+
+  //mux := http.NewServeMux()
+  //mux.HandleFunc("/", home)
+  //mux.HandleFunc("/grid.html", grid)
+  //mux.HandleFunc("/trade.html", trade)
+  //mux.HandleFunc("/chat.html", chat)
+  //mux.HandleFunc("/click.go", click)
+  //mux.HandleFunc("/snippet", showSnippet)
+  //mux.HandleFunc("/snippet/create", createSnippet)
+
+  //http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+  //myMux := mux.NewRouter()
+  //myMux.HandleFunc("/", home)
+  //myMux.HandleFunc("/index.html", home)
+  //myMux.HandleFunc("/grid.html", grid)
+  //myMux.HandleFunc("/trade.html", trade)
+  //myMux.HandleFunc("/chat.html", chat)
+  //myMux.HandleFunc("/click.go", click)
+  //myMux.HandleFunc("/snippet", showSnippet)
+  //myMux.HandleFunc("/snippet/create", createSnippet)
+  //http.Handle("/", myMux)
+
+  //http.HandleFunc("/", home)
+  //http.HandleFunc("/index/", home)
+  //http.HandleFunc("/grid/", grid)
+  //http.HandleFunc("/trade/", trade)
+  //http.HandleFunc("/chat/", chat)
+
   fs := http.FileServer(http.Dir("internal/ui"))
   http.Handle("/", fs)
+  http.Handle("/index.html", fs)
   http.Handle("/grid.html", fs)
   http.Handle("/trade.html", fs)
   http.Handle("/chat.html", fs)
 
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", home)
-  mux.HandleFunc("/grid.html", grid)
-  mux.HandleFunc("/trade.html", trade)
-  mux.HandleFunc("/chat.html", chat)
-
   log.Println("Starting server on :" + os.Getenv("PORT"))
-  err := http.ListenAndServe(os.Getenv("CHROMEHOST") + ":" + os.Getenv("PORT"), mux)
+  err := http.ListenAndServe(os.Getenv("CHROMEHOST") + ":" + os.Getenv("PORT"), nil)
+  //err := http.ListenAndServe(os.Getenv("CHROMEHOST") + ":" + os.Getenv("PORT"), mux)
+  //err := http.ListenAndServe(os.Getenv("CHROMEHOST") + ":" + os.Getenv("PORT"), myMux)
   log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/priscillashort/trade-wars
 
 go 1.13.1
+
+require github.com/gorilla/mux v1.7.3
