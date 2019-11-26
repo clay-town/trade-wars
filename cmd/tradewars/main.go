4878239ab2110e60eac3678f11a13b14e123dd50
clay-town <justinmccune@icloud.com>
2019-11-26 11:14:16 -0800

    Modify main.go for universal use

--- cmd/tradewars/main.go
+++ cmd/tradewars/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
-    "github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-  fs := http.FileServer(http.Dir("./internal/ui/static/"))
-  mux := http.NewServeMux()
-  mux.HandleFunc("/players", playersHandler)
-  mux.HandleFunc("/", homeHandler)
-  mux.HandleFunc("/index.html", homeHandler)
-  mux.HandleFunc("/map.html", mapHandler)
-  mux.HandleFunc("/map", mapHandler)
-  mux.HandleFunc("/trade.html", tradeHandler)
-  mux.HandleFunc("/chat.html", chatHandler)
-  mux.Handle("/static/", http.StripPrefix("/static", fs))
+	fs := http.FileServer(http.Dir("./internal/ui/static/"))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/players", playersHandler)
+	mux.HandleFunc("/", homeHandler)
+	mux.HandleFunc("/index.html", homeHandler)
+	mux.HandleFunc("/map.html", mapHandler)
+	mux.HandleFunc("/map", mapHandler)
+	mux.HandleFunc("/trade.html", tradeHandler)
+	mux.HandleFunc("/chat.html", chatHandler)
+	mux.Handle("/static/", http.StripPrefix("/static", fs))
 
-  godotenv.Load()
+	godotenv.Load()
 
-  log.Println("Starting server on :" + os.Getenv("PORT"))
-  err := http.ListenAndServe(os.Getenv("CHROMEHOST") + ":" + os.Getenv("PORT"), mux)
-  log.Fatal(err)
+	log.Println("Starting server on " + os.Getenv("PORT"))
+	err := http.ListenAndServe(os.Getenv("PORT"), mux)
+	log.Fatal(err)
 }
