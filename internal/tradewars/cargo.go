997aa2e91d7bf7de5268b296bc7a88da2fb0892b
Justin McCune <justinmccune@icloud.com>
2019-12-05 13:30:49 -0800

    Improve station inventory

--- internal/tradewars/cargo.go
+++ internal/tradewars/cargo.go
@@ -3,4 +3,5 @@ package tradewars
 type Cargo struct {
     Item string `json:"item"`
     Quantitiy string `json:"quantitiy"`
+    Unitcost int `json:"unitcost"`
 }
--- internal/tradewars/station.go
+++ internal/tradewars/station.go
@@ -3,4 +3,5 @@ package tradewars
 type Station struct {
     Designation string `json:"designation"`
     Location string `json:"location"`
+    Cargos []Cargo `json:"cargos"`
 }
