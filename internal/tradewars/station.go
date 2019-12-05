bc117b326e9530795fab51a2310641266f828691
Justin McCune <justinmccune@icloud.com>
2019-12-05 13:34:53 -0800

    Finish station struct

--- internal/tradewars/station.go
+++ internal/tradewars/station.go
@@ -3,5 +3,5 @@ package tradewars
 type Station struct {
     Designation string `json:"designation"`
     Location string `json:"location"`
-    Cargos []Cargo `json:"cargos"`
+    Cargos []Cargo `json:"cargos"`   
 }
